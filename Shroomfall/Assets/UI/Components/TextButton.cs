37ed8c213846808617627d70e0cc2c4dd2807396
TiepDaCoder <tombin082005@gmail.com>
2026-08-12 21:08:41 +0700

    add: Main Menu MVP and TextButton

---
+++ Shroomfall/Assets/UI/Components/TextButton.cs
@@ -0,0 +1,28 @@
+﻿using TMPro;
+using UnityEngine.UI;
+
+public class TextButton : Button
+{
+    #region Attributes
+    private TMP_Text label;
+    #endregion
+
+    #region Properties
+    #endregion
+
+    #region Methods
+    protected override void Awake()
+    {
+        base.Awake();
+
+        label = GetComponentInChildren<TMP_Text>();
+    }
+
+    public void SetText(
+        string value)
+    {
+        if (label == null) return;
+        label.text = value;
+    }
+    #endregion
+}
---
+++ Shroomfall/Assets/UI/MainMenu/MainMenuPresenter.cs
@@ -0,0 +1,151 @@
+﻿using Assets.Enums;
+using Assets.Services;
+using Assets.Utilities;
+using System;
+using UnityEngine;
+
+namespace Assets.UI.MainMenu
+{
+    public class MainMenuPresenter
+    {
+        #region Attributes
+        private readonly GameService gameService;
+        private readonly UIService uiService;
+        private readonly MainMenuView mainMenuView;
+
+        private bool disposed;
+        #endregion
+
+        #region Properties
+        #endregion
+
+        public MainMenuPresenter(
+            GameService gameService,
+            UIService uiService,
+            MainMenuView mainMenuView)
+        {
+            this.gameService = gameService;
+            this.uiService = uiService;
+            this.mainMenuView = mainMenuView;
+
+            Bind();
+        }
+
+        #region Methods
+        public void Dispose()
+        {
+            if (disposed) return;
+            disposed = true;
+
+            // Outbound
+            uiService.OnUIStateChanged -= OnUIStateChanged;
+            uiService.OnGlobalInteractableChanged -= mainMenuView.SetInteractable;
+
+            // Inbound
+            mainMenuView.OnSteamAuthClicked -= OnSteamAuthClicked;
+            mainMenuView.OnSignInClicked -= OnSignInClicked;
+            mainMenuView.OnSignUpClicked -= OnSignUpClicked;
+            mainMenuView.OnSettingClicked -= OnSettingClicked;
+            mainMenuView.OnQuitClicked -= OnQuitClicked;
+            mainMenuView.OnInstagramClicked -= OnInstagramClicked;
+            mainMenuView.OnFacebookClicked -= OnFacebookClicked;
+            mainMenuView.OnTiktokClicked -= OnTiktokClicked;
+            mainMenuView.OnDiscordClicked -= OnDiscordClicked;
+            mainMenuView.OnYoutubeClicked -= OnYoutubeClicked;
+        }
+
+        private void Bind()
+        {
+            if (disposed)
+                throw new ObjectDisposedException(nameof(MainMenuPresenter));
+
+            // Outbound
+            uiService.OnUIStateChanged += OnUIStateChanged;
+            uiService.OnGlobalInteractableChanged += mainMenuView.SetInteractable;
+
+            // Inbound
+            mainMenuView.OnSteamAuthClicked += OnSteamAuthClicked;
+            mainMenuView.OnSignInClicked += OnSignInClicked;
+            mainMenuView.OnSignUpClicked += OnSignUpClicked;
+            mainMenuView.OnSettingClicked += OnSettingClicked;
+            mainMenuView.OnQuitClicked += OnQuitClicked;
+            mainMenuView.OnInstagramClicked += OnInstagramClicked;
+            mainMenuView.OnFacebookClicked += OnFacebookClicked;
+            mainMenuView.OnTiktokClicked += OnTiktokClicked;
+            mainMenuView.OnDiscordClicked += OnDiscordClicked;
+            mainMenuView.OnYoutubeClicked += OnYoutubeClicked;
+        }
+
+        private void OnUIStateChanged(
+            UIService state)
+        {
+            mainMenuView.SetVisible(state.ShowMainMenu);
+        }
+
+        private void OnSteamAuthClicked()
+        {
+            AsyncHelper.Run(uiService, async () =>
+            {
+                await gameService.PlayerSteamAuthenticate();
+            });
+        }
+
+        private void OnSignInClicked()
+        {
+            gameService.PushPhase(GamePhase.SignIn);
+        }
+
+        private void OnSignUpClicked()
+        {
+            gameService.PushPhase(GamePhase.SignUp);
+        }
+
+        private void OnSettingClicked()
+        {
+            gameService.PushPhase(GamePhase.SettingMenu);
+        }
+
+        private void OnQuitClicked()
+        {
+            AsyncHelper.Run(uiService, async () =>
+            {
+                await gameService.QuitGame();
+            });
+        }
+
+        private void OnInstagramClicked()
+        {
+            OpenSocialLink(Configuration.INSTAGRAM_URL);
+        }
+
+        private void OnFacebookClicked()
+        {
+            OpenSocialLink(Configuration.FACEBOOK_URL);
+        }
+
+        private void OnTiktokClicked()
+        {
+            OpenSocialLink(Configuration.TIKTOK_URL);
+        }
+
+        private void OnDiscordClicked()
+        {
+            OpenSocialLink(Configuration.DISCORD_URL);
+        }
+
+        private void OnYoutubeClicked()
+        {
+            OpenSocialLink(Configuration.YOUTUBE_URL);
+        }
+
+        private static void OpenSocialLink(
+            string url)
+        {
+            if (string.IsNullOrWhiteSpace(url))
+                return;
+
+            Application.OpenURL(url);
+        }
+        #endregion
+    }
+}
