fdc2034027142971d2fd2c95e0aba86d4f7f2df5
TiepDaCoder <tombin082005@gmail.com>
2026-08-13 21:14:58 +0700

    add: Sign in, Setting menu and related components, models with data (LocaleCatalogSO)

---
+++ Shroomfall/Assets/UI/Components/SelectorItem.cs
@@ -0,0 +1,47 @@
+﻿using Assets.UI.Models;
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SelectorItem : MonoBehaviour
+{
+    #region Attributes
+    [Header("Button Components")]
+    [SerializeField] private Image icon;
+    [SerializeField] private Button button;
+
+    [Header("Visual States")]
+    [SerializeField] private Sprite normalSprite;
+    [SerializeField] private Sprite selectedSprite;
+    #endregion
+
+    #region Properties
+    public event Action OnSelect;
+    public bool IsSelected { get; private set; }
+    public IconModel Model { get; private set; }
+    #endregion
+
+    #region Methods
+    public void Bind(
+        IconModel model)
+    {
+        Model = model;
+        icon.sprite = model.Icon;
+
+        button.onClick.RemoveAllListeners();
+        button.onClick.AddListener(() => OnSelect?.Invoke());
+
+        SetSelected(false);
+    }
+
+    public void SetSelected(
+        bool state)
+    {
+        IsSelected = state;
+        if (button.image != null)
+        {
+            button.image.sprite = state ? selectedSprite : normalSprite;
+        }
+    }
+    #endregion
+}
---
+++ Shroomfall/Assets/UI/Models/IconModel.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+namespace Assets.UI.Models
+{
+    public class IconModel
+    {
+        public string Id;
+        public Sprite Icon;
+        public string Name;
+        public string Description;
+    }
+}
