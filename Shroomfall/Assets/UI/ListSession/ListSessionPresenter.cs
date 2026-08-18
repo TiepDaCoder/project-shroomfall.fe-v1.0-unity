53165814a7afa8b3165bc63ce2bb7a833af01f6c
TiepDaCoder <tombin082005@gmail.com>
2026-08-18 20:26:23 +0700

    add: MVP for ListSession and related Components(EntityPreviewView), Data(EntityCalalogSO), Model(SessionEntryModel) and Utilities(DirectionHelper)

---
+++ Shroomfall/Assets/UI/ListSession/ListSessionPresenter.cs
@@ -0,0 +1,99 @@
+﻿using Assets.Enums;
+using Assets.Services;
+using Assets.Utilities;
+using System;
+
+namespace Assets.UI.ListSession
+{
+    public class ListSessionPresenter : IDisposable
+    {
+        #region Attributes
+        private readonly UIService uiService;
+        private readonly SessionService sessionService;
+        private readonly GameService gameService;
+        private readonly ListSessionView listSessionView;
+
+        private bool disposed;
+        #endregion
+
+        #region Properties
+        #endregion
+
+        public ListSessionPresenter(
+            UIService uiService,
+            SessionService sessionService,
+            GameService gameService,
+            ListSessionView listSessionView)
+        {
+            this.uiService = uiService;
+            this.sessionService = sessionService;
+            this.gameService = gameService;
+            this.listSessionView = listSessionView;
+
+            Bind();
+        }
+
+        #region Methods
+        public void Dispose()
+        {
+            if (disposed) return;
+            disposed = true;
+
+            // Outbound
+            uiService.OnUIStateChanged -= OnUIStateChanged;
+            uiService.OnGlobalInteractableChanged -= listSessionView.SetInteractable;
+
+            // Inbound
+            listSessionView.OnBackClicked -= OnBackClicked;
+            listSessionView.OnCreateClicked -= OnCreateClicked;
+            listSessionView.OnLoadClicked -= OnLoadClicked;
+        }
+
+        private void Bind()
+        {
+            if (disposed)
+                throw new ObjectDisposedException(nameof(ListSessionPresenter));
+
+            // Outbound
+            uiService.OnUIStateChanged += OnUIStateChanged;
+            uiService.OnGlobalInteractableChanged += listSessionView.SetInteractable;
+
+            // Inbound
+            listSessionView.OnBackClicked += OnBackClicked;
+            listSessionView.OnCreateClicked += OnCreateClicked;
+            listSessionView.OnLoadClicked += OnLoadClicked;
+        }
+
+        private void OnUIStateChanged(
+            UIService service)
+        {
+            listSessionView.SetVisible(service.ShowListSession);
+
+            AsyncHelper.Run(uiService, async () =>
+            {
+                var session = await sessionService.FetchSessions();
+                listSessionView.SetSessions(session.Sessions);
+            });
+        }
+
+        private void OnBackClicked()
+        {
+            gameService.BackToMenu();
+        }
+
+        private void OnCreateClicked()
+        {
+            gameService.PushPhase(GamePhase.CreateSession);
+        }
+
+        private void OnLoadClicked(
+            string sessionId)
+        {
+            AsyncHelper.Run(uiService, async () =>
+            {
+                await gameService.LoadSessionAndWorld(sessionId);
+            });
+        }
+        #endregion
+    }
+}
---
+++ Shroomfall/Assets/UI/Models/SessionEntryModel.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace Assets.UI.Models
+{
+    public class SessionEntryModel
+    {
+        public string SessionId;
+        public EntityAsset Entity;
+        public Color SkinColor;
+    }
+}
